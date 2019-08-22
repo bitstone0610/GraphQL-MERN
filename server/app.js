1d8f752d4c72bf930c798f15de50b51b9cb1ec3c
bitstone0610 <bitstone0610@outlook.com>
2019-08-22 22:35:37 +0200

    mongoose connection

--- server/app.js
+++ server/app.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+
+const connectionString = 'mongodb+srv://frost:%21%21%21apollo725%40%40%40@cluster0-mf75a.mongodb.net/test?retryWrites=true&w=majority';
+
+mongoose.connect(connectionString, { useNewUrlParser: true } );
+mongoose.connection.once('open', () => {
+  console.log("connected to database");
+})
 
 const app = express();
 
